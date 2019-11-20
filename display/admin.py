152a94f7f94bcdee2bcb5d2863ff6149a349bc20
Wh1isper <43375501+Wh1isper@users.noreply.github.com>
2019-11-20 13:38:56 +0800

    修改后台管理头

--- display/admin.py
+++ display/admin.py
@@ -4,20 +4,27 @@ from django.contrib import admin
 
 from .models import Event,User
 
+# from django.contrib.auth.models import Group
+# from django.contrib.auth.models import User as admin_User
+
+# admin.site.unregister(Group)
+# admin.site.unregister(admin_User)
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('user_name',)
     search_fields = ['user_name',]
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display =('CREATE_TIME','DISTRICT_NAME','STREET_NAME','COMMUNITY_NAME','EVENT_TYPE_NAME',
                    'MAIN_TYPE_NAME','SUB_TYPE_NAME','DISPOSE_UNIT_NAME','EVENT_SRC_NAME','EVENT_PROPERTY_NAME')
     list_filter = ['CREATE_TIME','DISTRICT_NAME','STREET_NAME','COMMUNITY_NAME','EVENT_TYPE_NAME',
                    'MAIN_TYPE_NAME','SUB_TYPE_NAME','DISPOSE_UNIT_NAME','EVENT_SRC_NAME','EVENT_PROPERTY_NAME']
-    list_per_page = 500
+    list_per_page = 200
     search_fields = ['CREATE_TIME','DISTRICT_NAME','STREET_NAME','COMMUNITY_NAME','EVENT_TYPE_NAME',
                    'MAIN_TYPE_NAME','SUB_TYPE_NAME','DISPOSE_UNIT_NAME','EVENT_SRC_NAME','EVENT_PROPERTY_NAME']
     date_hierarchy = "CREATE_TIME"
 
-admin.site.site_header = '民生监测系统后台'
-admin.site.site_title = '民生检测系统后台'
+admin.site.site_header = '政府大数据实时治理分析系统后台管理'
+admin.site.site_title = '政府大数据实时治理分析系统后台管理'
--- display/apps.py
+++ display/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DisplayConfig(AppConfig):
     name = 'display'
-    verbose_name = '民生监测系统'
+    verbose_name = '政府大数据实时治理分析系统网站管理'
